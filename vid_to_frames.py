388a1d924ea0720d4c9fc936c34c2fc41829c754
Onyeogulu Tochukwu <onyeogulutochukwu@gmail.com>
2022-06-08 21:11:59 +0100

    add videoconveter

--- vid_to_frames.py
+++ vid_to_frames.py
@@ -1,10 +1,10 @@
 
 import cv2
-vidcap = cv2.VideoCapture('world.mp4')
+vidcap = cv2.VideoCapture('/mnt/mars-beta/tochukwu/maestro/maestro_code/Endo_video/Endoscope-Video2.avi')
 success,image = vidcap.read()
 count = 0
 while success:
-  cv2.imwrite("frames/frame%d.jpg" % count, image)     # save frame as JPEG file      
+  cv2.imwrite("/mnt/mars-beta/tochukwu/maestro/maestro_code/frames/frame%d.jpg" % count, image)     # save frame as JPEG file      
   success,image = vidcap.read()
   print('Read a new frame: ', success)
   count += 1
