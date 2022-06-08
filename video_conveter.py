3e30232b0ef2a989e83ed07d79a4f140da8570eb
Onyeogulu Tochukwu <onyeogulutochukwu@gmail.com>
2022-06-08 21:31:54 +0100

    add new folder

--- video_conveter.py
+++ video_conveter.py
@@ -3,12 +3,12 @@ import argparse
 import cv2
 
 count = 0
-vidcap = cv2.VideoCapture('/mnt/mars-beta/tochukwu/maestro/maestro_code/Endo_video/Endoscope-Video2.avi')
+vidcap = cv2.VideoCapture('/mnt/mars-beta/tochukwu/maestro/maestro_code/Endo_video/Endoscope-Video.avi')
 success,image = vidcap.read()
 success = True
 while success:
     vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line 
     success,image = vidcap.read()
     print ('Read a new frame: ', success)
-    cv2.imwrite("/mnt/mars-beta/tochukwu/maestro/maestro_code/newframe/frame%d.jpg" % count, image)     # save frame as JPEG file
+    cv2.imwrite("/mnt/mars-beta/tochukwu/maestro/maestro_code/newframe2/frame%d.jpg" % count, image)     # save frame as JPEG file
     count = count + 1
