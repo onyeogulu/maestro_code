423aeb7a3c5917cb348fc29da4fa691f70978b0e
salmank255 <salmank255@gmail.com>
2022-06-12 23:03:56 +0100

    added frame skip

--- vid_to_frames.py
+++ vid_to_frames.py
@@ -2,9 +2,12 @@
 import cv2
 vidcap = cv2.VideoCapture('world.mp4')
 success,image = vidcap.read()
-count = 0
+count = 1
+skip = 30
+success = True
 while success:
-  cv2.imwrite("frames/frame%d.jpg" % count, image)     # save frame as JPEG file      
+  if count%skip == 0:
+    cv2.imwrite("frames/frame%d.jpg" % count, image)     # save frame as JPEG file      
   success,image = vidcap.read()
   print('Read a new frame: ', success)
   count += 1
