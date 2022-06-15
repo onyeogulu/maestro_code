b9163a3d2899a9f26faafc6a3a5e35858bdc0175
Onyeogulu Tochukwu <onyeogulutochukwu@gmail.com>
2022-06-15 13:17:24 +0100

    up

--- vid_to_frames.py
+++ vid_to_frames.py
@@ -1,13 +1,13 @@
 
 import cv2
-vidcap = cv2.VideoCapture('/mnt/mars-beta/tochukwu/maestro/maestro_code/Endo_video/Endoscope-Video2.avi')
+vidcap = cv2.VideoCapture('/mnt/mars-beta/tochukwu/maestro/maestro_code/newData/eyetracker/Endoscope-Video.avi')
 success,image = vidcap.read()
 count = 1
-skip = 30
+skip = 10000
 success = True
 while success:
   if count%skip == 0:
-    cv2.imwrite("frames/frame%d.jpg" % count, image)     # save frame as JPEG file      
+    cv2.imwrite("/mnt/mars-beta/tochukwu/maestro/maestro_code/Videotoframe2/frame%d.jpg" % count, image)     # save frame as JPEG file      
   success,image = vidcap.read()
   print('Read a new frame: ', success)
   count += 1
