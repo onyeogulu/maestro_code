15cb868b5e6683de498904d7fdf94841d4130cab
Onyeogulu Tochukwu <onyeogulutochukwu@gmail.com>
2022-06-23 15:16:22 +0100

    commit changes

--- vid_to_frames.py
+++ vid_to_frames.py
@@ -1,13 +1,14 @@
 
 import cv2
-vidcap = cv2.VideoCapture('/mnt/mars-beta/tochukwu/maestro/maestro_code/newData/eyetracker/Endoscope-Video.avi')
+vidcap = cv2.VideoCapture('/mnt/mars-beta/tochukwu/maestro/maestro_code/newData/eyetracker/world.mp4')
 success,image = vidcap.read()
 count = 1
-skip = 10000
+skip = 30
 success = True
 while success:
   if count%skip == 0:
-    cv2.imwrite("/mnt/mars-beta/tochukwu/maestro/maestro_code/Videotoframe2/frame%d.jpg" % count, image)     # save frame as JPEG file      
+    cv2.imwrite("/mnt/mars-beta/tochukwu/maestro/maestro_code/NewBodyCam/frame%d.jpg" % count, image)     # save frame as JPEG file      
   success,image = vidcap.read()
   print('Read a new frame: ', success)
   count += 1
+  
--- video_conveter.py
+++ video_conveter.py
@@ -3,12 +3,12 @@ import argparse
 import cv2
 
 count = 0
-vidcap = cv2.VideoCapture('/mnt/mars-beta/tochukwu/maestro/maestro_code/Endo_video/Endoscope-Video.avi')
+vidcap = cv2.VideoCapture('/mnt/mars-beta/tochukwu/maestro/maestro_code/newData/eyetracker/world.mp4')
 success,image = vidcap.read()
 success = True
 while success:
-    vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line 
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000)) # added this line 
     success,image = vidcap.read()
     print ('Read a new frame: ', success)
-    cv2.imwrite("/mnt/mars-beta/tochukwu/maestro/maestro_code/newframe2/frame%d.jpg" % count, image)     # save frame as JPEG file
+    cv2.imwrite("/mnt/mars-beta/tochukwu/maestro/maestro_code/BodycamImage/frame%d.jpg" % count, image) # save frame as JPEG file
     count = count + 1
